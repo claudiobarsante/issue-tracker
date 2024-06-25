e52c6735ee603213f17d04e6d942d13e6c5dc6c2
Claudio Barsante <clbmribas@gmail.com>
2024-06-25 13:21:35 -0300

    feat: add <Skeleton/> to issue detail page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,22 @@
+import { Box, Card, Flex } from "@radix-ui/themes";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingIssueDetailpage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gapX="3">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      {/* prose class is from the '@tailwindcss/typography' plugin */}
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailpage;

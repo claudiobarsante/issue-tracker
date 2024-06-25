95979048f22afdf719f862cb51f2c85c15559fdc
Claudio Barsante <clbmribas@gmail.com>
2024-06-25 12:59:54 -0300

    refactor: build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,8 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
-
-import Link from "next/link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import Link from "../components/Link";
 import IssuesActions from "./IssuesActions";
 
 const IssuesPage = async () => {

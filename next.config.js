52b9a585f77eb9852c2b4677a1e1c72a5dbcbb57
Claudio Barsante <clbmribas@gmail.com>
2024-08-05 12:20:58 -0300

    fix: next.config.js - add headers to avoid forbidden error when accessing the avatar of the user from the Google account

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // - to avoid forbidden error when accessing the avatar of the user from the Google account
+  async headers() {
+    return [
+      {
+        source: "/:path*", // -- * all enpoints of the application
+        headers: [{ key: "referrer-policy", value: "no-referrer" }],
+      },
+    ];
+  },
+  //--
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
